3195770130f966e021c06d193902c22559bca8ee
Kolmagorov Ivan <ivandevastator27@gmail.com>
2023-02-08 14:40:43 +0300

    1

---
+++ 1/Program.cs
@@ -0,0 +1,29 @@
+﻿double[,] CreateArray()
+{
+    double[,] array = new double[3, 4];
+    Random rnd = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int g = 0; g < array.GetLength(1); g++)
+        {
+            array[i, g] = Convert.ToDouble(new Random().Next(-100, 100)) / 10;
+        }
+    }
+    return array;
+}
+
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write("[ ");
+        for (int g = 0; g < array.GetLength(1); g++)
+        {
+            Console.Write(array[i, g] + " ");
+        }
+        Console.Write("]");
+        Console.WriteLine("");
+    }
+}
+double[,] result = CreateArray();
+PrintArray(result);
